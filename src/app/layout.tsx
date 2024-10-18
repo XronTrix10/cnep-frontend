48fb6738b09d4738d43d12cbe9abb3468c863a7f
Xron Trix <xrontrix@gmail.com>
2024-10-18 15:38:00 +0530

    feat: added custom theme font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
+import { Noto_Sans, Pacifico } from "next/font/google";
 import "@/styles/main.css";
+import { cn } from "@/lib/utils/cn";
 
 const noto_sans = Noto_Sans({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-noto",
+});
+
+const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-pacifico",
 });
 
 export const metadata: Metadata = {
@@ -20,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto_sans.variable}>{children}</body>
+      <body className={cn(noto_sans.variable, pacifico.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       },
       fontFamily: {
         noto: ["var(--font-noto)"],
+        pacifico : ["var(--font-pacifico)"]
       }
     },
   },
